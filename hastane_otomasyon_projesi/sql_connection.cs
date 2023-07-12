ba51da8ff8380534581d3f4effecbddce69819ef
ipekdural <duralipek6@gmail.com>
2023-07-12 09:13:30 +0300

    Add project files.

---
+++ hastane_otomasyon_projesi/sql_connection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace hastane_otomasyon_projesi
+{
+    internal class sql_connection
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection conn = new SqlConnection("");
+            conn.Open();
+            return conn;
+        }
+
+    }
+}
